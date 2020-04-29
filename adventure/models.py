4e899693428b450e3ae17242b394f83710f18043
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-04-29 12:50:33 -0600

    setup beginnings of rooms endpoint

--- adventure/models.py
+++ adventure/models.py
@@ -12,6 +12,8 @@ class Room(models.Model):
     s_to = models.IntegerField(default=0)
     e_to = models.IntegerField(default=0)
     w_to = models.IntegerField(default=0)
+    def listRooms():
+        return [{"title": item.title, "description": item.description} for item in Room.objects.all()]
     def connectRooms(self, destinationRoom, direction):
         destinationRoomID = destinationRoom.id
         try:
